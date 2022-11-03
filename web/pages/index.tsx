d3ea2d598f4b5a84abe7bac77517ceaa5069471e
Dirceu Rezende <dirceu.srezende@gmail.com>
2022-11-03 20:11:41 -0300

    feat: initial structure

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <h1>Hello NLW</h1>;
+type HomeProps = {
+  count: number;
+};
+
+export default function Home({ count }: HomeProps) {
+  return <h1>Contagem {count}</h1>;
 }
+
+export const getServerSideProps = async () => {
+  const response = await fetch("http://localhost:3333/pools/count");
+  const data = await response.json();
+
+  return {
+    props: {
+      count: data.count,
+    },
+  };
+};
